3f0309d647acd2bb8d95ecd5da80bd92dd6fd25d
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 13:01:58 +0530

    Solution for Day 14!

---
+++ Day 14/Scope/Solution.java
@@ -0,0 +1,11 @@
+
+Difference(int[] a){
+            elements = a;
+        }
+
+        public void computeDifference() {
+            Arrays.sort(elements);
+            maximumDifference = Math.abs(elements[elements.length-1] - elements[0]);
+        }
+    
+
