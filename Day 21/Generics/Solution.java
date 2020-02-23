f17f07bb71dae9a44068aaa9013766e990cf2180
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 13:08:36 +0530

    Solution for Day 21!

---
+++ Day 21/Generics/Solution.java
@@ -0,0 +1,16 @@
+
+    /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    static <E>  void printArray( E[] inputArray)
+{
+for( E e : inputArray)
+    {
+    System.out.println(""+e);
+}
+
+}
+   
+
